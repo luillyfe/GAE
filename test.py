2c9df8b5eca6b3a2972e4a69b70fe373ad0b6e35
luillyfe <luillyfe89@gmail.com>
2014-08-06 05:01:00 -0500

    Registrar candidatos y reuniones.

--- test.py
+++ test.py
@@ -1,33 +1,22 @@
 
-# -*- coding: utf-8 -*-
-import os
-import urllib
 import webapp2
 
-from google.appengine.ext import blobstore
-from google.appengine.ext.webapp import blobstore_handlers
+class Main( webapp2.RequestHandler ):
+	def get(self):
+		self.response.write('hola')
 
-class MainHandler(webapp2.RequestHandler):
-  def get(self):
-    upload_url = blobstore.create_upload_url('/upload')
-    self.response.out.write('<html><body>')
-    self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
-    self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
-        name="submit" value="Submit"> </form></body></html>""")
+	def put(self):
+		self.response.write( 'funciono!!' )
 
-class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
-  def post(self):
-    upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
-    blob_info = upload_files[0]
-    self.redirect('/serve/%s' % blob_info.key())
+"""def post(self):
+		picture = self.get_uploads('file')[0]
 
-class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
-  def get(self, resource):
-    resource = str(urllib.unquote(resource))
-    blob_info = blobstore.BlobInfo.get(resource)
-    self.send_blob(blob_info)
+		self.redirect( '/users/img/%s' % picture.key() )"""
 
-app = webapp2.WSGIApplication([('/', MainHandler),
-                               ('/upload', UploadHandler),
-                               ('/serve/([^/]+)?', ServeHandler)],
-                              debug=True)
+
+
+
+
+application = webapp2.WSGIApplication([
+	webapp2.Route('/', handler=Main, handler_method='put'),
+	], debug=True)
